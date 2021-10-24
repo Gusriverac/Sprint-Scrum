5ef66816c52ec42b8d85e4fa5297cef3aaa7cdba
Gusriverac <50466375+Gusriverac@users.noreply.github.com>
2021-10-24 00:35:49 -0500

    Add files via upload

---
+++ src/components/Menu/header.js
@@ -0,0 +1,11 @@
+import Navbar from './nav';
+
+const header = () => {
+    return (
+        <header style={{background:'red'}}>
+            <Navbar/>
+        </header>
+    );
+};
+
+export default header;
---
+++ src/components/Menu/index.js
@@ -0,0 +1,8 @@
+import Header from 'components/Menu/header';
+// import style from './style.css';
+
+const Menu = () => {
+    return (<Header />);
+} 
+
+export default Menu;
